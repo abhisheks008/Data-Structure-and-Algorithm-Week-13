bb08c5a89b7ac857cfc6afe69b49c669f3e13bb3
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2020-11-10 18:57:35 +0530

    Create Week 13 Q3.py

---
+++ Week 13 Q3.py
@@ -0,0 +1,9 @@
+test = int(input())
+elements = int(input())
+string = ['2','1','3','2','3','1']
+for i in range (0,test):
+    check = input().split(" ")
+    if (check==string):
+        print("y")
+    else :
+        print("n")
